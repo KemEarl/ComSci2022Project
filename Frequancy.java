bba9b51c3bcb8ca451acd34d44454698f7bae5e7
zaza-germond <100357139+zaza-germond@users.noreply.github.com>
2022-09-28 10:51:31 +0200

    Add files via upload
    This is the frequency class that has a method that checks if all letters are used and a method that generates a frequency int array in the order of the char list that is passed to the class.

---
+++ Frequancy.java
@@ -0,0 +1,40 @@
+public class Frequancy(
+	private int[] freq;
+	private char[] word;//might not need this one
+	private char[] letarray;
+	
+	public Frequancy(String words,char letarrays){
+		this.letarray=letarrays;
+		this.word=words.toCharArray();
+	}
+	public void checkword() {
+		int cnt=0;
+		int[] count= Frequency();
+		
+		for (int k=0;k<count.length;k++){
+			System.out.print(count[k]);
+			if (count[k]==0){
+				System.out.println("Not all letters used");
+			}
+			cnt=cnt+count[k];
+		}
+		if (cnt==0){
+			System.out.println("None of the letters are used");
+			}
+		
+	}//checks if all letters are present atleast once 
+	
+	public  int[] Frequency(){
+		int[] Countarray=new int[this.letarray.length];
+		for (int i=0;i< this.letarray.length;i++) {
+			for(int j=0;j< this.word.length;j++)
+			if (this.letarray[i]==this.word[j]){
+				Countarray[i]=Countarray[i]+1;
+			}
+
+		}
+		return Countarray;
+	}//creates a histrogram which checks the frequancy of letters returns a int array with frequancy in order of char list of the letters provided
+}
+
+}
