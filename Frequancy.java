ac95c377ca65e06a51cc834357d0724fc87af969
zaza-germond <100357139+zaza-germond@users.noreply.github.com>
2022-10-02 16:46:03 +0200

    Update Frequancy.java

--- Frequancy.java
+++ Frequancy.java
@@ -1,15 +1,34 @@
-public class Frequancy(
+public class Frequancy{
 	private int[] freq;
 	private char[] word;//might not need this one
 	private char[] letarray;
 	
-	public Frequancy(String words,char letarrays){
+	public Frequancy(String words,char[] letarrays){
 		this.letarray=letarrays;
 		this.word=words.toCharArray();
 	}//constructor
 	
+	public Frequancy(String[] dict){
+		this.freq= new int[26];
+		
+		for (int i = 0; i < dict.length; i++){
+			this.word=dict[i].toCharArray();
+			for (int j = 0; j < this.word.length; j++){
+				if ((this.word[j]>=97) && (this.word[j] <= 123)){
+					this.freq[this.word[j] - 97]++;
+				}
+			}
+		}
+	}//constructor
+	
+	public int[] getArray(){
+		return this.freq;
+	}
+	
+	
 	public void checkword() {
 		int cnt=0;
+		
 		int[] count= Frequency();
 		
 		for (int k=0;k<count.length;k++){
@@ -36,6 +55,8 @@ public class Frequancy(
 		}
 		return Countarray;
 	}//returns a int array with frequancy of letters in order of char list of the letters provided
+	
+	
+	
 }
 
-}
