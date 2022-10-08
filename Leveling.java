4db1076bac8d7ae8aa4a8d0ac173b05919bc7981
zaza-germond <100357139+zaza-germond@users.noreply.github.com>
2022-10-08 17:10:25 +0200

    Update Leveling.java

--- Leveling.java
+++ Leveling.java
@@ -2,17 +2,20 @@ public class Leveling {
 
     private static int pts_req=0;
     private static int tempts=0;
-    private static String [] let_me;
-
+private static String [] let_me ;
+    private static Array [] let_mes = {{"N","T","A","R","I","G","C"},{"A","S","O","H","W","P","L"},{"M","E","L","S","R","U","D"},{"G","A","C","S","F","D","H"),{"I","T","D","F","A","O","E"}{"C","P","S","H","R","E","O"},{"M","S","Y",,"A","T","R","E"},{"K","N","I","S","E","F","G"},{"O","U","V","L","B","D","N"},{"L","B","F","E","S","A","P"}};
+    //double check this intialisation
     public Leveling() {
         pts_req = 0;
         tempts = 0;
+	    
     }
 	
     public void FixedLeveling(int Lvl) {
         if (Lvl <= 10) {
 		this.pts_req+=100;
 		this.tempts+=3;
+		this.let_me=let_mes[Lvl-1];
         }
     }
 
