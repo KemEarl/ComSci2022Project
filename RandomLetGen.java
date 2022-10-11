9df6562fbff384339ccfe6bc6b53caecae63bf6b
KemEarl <68971765+KemEarl@users.noreply.github.com>
2022-10-11 20:28:09 +0200

    Update RandomLetGen.java

--- RandomLetGen.java
+++ RandomLetGen.java
@@ -38,8 +38,6 @@ public class RandomLetGen {
     String [] Let = {"A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L"
     ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T" ,"U" ,"V" ,"W" ,"X" ,"Y" ,"Z"};
 
-    String [] LetVow = {"A", "E", "I", "O", "U"};
-
     int [] LetIn = new int [LetNum];
     String [] LetOut = new String [LetNum];
 
@@ -53,30 +51,8 @@ public class RandomLetGen {
       LetOut[j] = Let[LetIn[j]];
     }
 
-    if (VowelCheck(LetOut, LetVow) == false) {
-      LetOut[Rando(LetNum)] = LetVow[Rando(5)];
-    } 
-
     RanLetArr = new String [LetNum];
     RanLetArr = LetOut;
     return RanLetArr;
-  } //Generates random letters
-  /////////////////////////////////////////////////////////////////////////////
-  public static boolean VowelCheck(String sub [], String Vows []) {
-    int far = sub.length;
-    int vow_far = sub.length;
-    int confNum = 0;
-    for (int k = 0; k < far; k++) {
-      for (int l = 0; l < vow_far; l++) {
-        if (sub[k] == Vows[l]) {
-          confNum++;
-        }
-      }
-    }
-    if (confNum == far) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-} //randomly picks vowel letters if random generated letters do not have vowels
+  } //Generates random letters 
+} 
