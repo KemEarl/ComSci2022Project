590c931828496b4cd7dbd5688f93c045f78cd51b
KemEarl <68971765+KemEarl@users.noreply.github.com>
2022-10-11 18:44:16 +0200

    Update Leveling.java

--- Leveling.java
+++ Leveling.java
@@ -1,34 +1,58 @@
 public class Leveling {
-
-    private static int pts_req=0;
-    private static int tempts=0;
-	private static String [] let_me ;
-    private static String [][] let_mes = {{"N","T","A","R","I","G","C"},{"A","S","O","H","W","P","L"},{"M","E","L","S","R","U","D"},{"G","A","C","S","F","D","H"},{"I","T","D","F","A","O","E"},{"C","P","S","H","R","E","O"},{"M","S","Y","A","T","R","E"},{"K","N","I","S","E","F","G"},{"O","U","V","L","B","D","N"},{"L","B","F","E","S","A","P"}};
+    private static int pts_req;
+    private static int tempts;
+    private static int level;
+    private static String [][] let_me = {{"N","T","A","R","I","G","C"},{"A","S","O","H","W","P","L"},{"M","E","L","S","R","U","D"},{"G","A","C","S","F","D","H"},{"I","T","D","F","A","O","E"},{"C","P","S","H","R","E","O"},{"M","S","Y","A","T","R","E"},{"K","N","I","S","E","F","G"},{"O","U","V","L","B","D","N"},{"L","B","F","E","S","A","P"}};
+    private static RandomLetGen only = new RandomLetGen();
+	private static String [] Letters;
     //double check this intialisation
+	/**
+	 * 
+	 */
 	public Leveling() {
         pts_req = 0;
         tempts = 0;
+        level = 1;
     }
 	
-    public void FixedLeveling(int Lvl) {
-		String[] store=new String[7];
-        if (Lvl <= 10) {
-		this.pts_req+=100;
-		this.tempts+=3;
-	
-		for(int i=0;i<8;i++){
-			store[i]=this.let_mes[Lvl-1][i];
-        }
-		this.let_me=store;
+    public String [] FixedLeveling() {
+        if (level <= 10) {
+		pts_req += 200;
+		tempts += 3;
+		Letters = let_me[level-1];
 		}
+        return Letters;
     }
 
-    public static void RandLeveling(int Lvl) {
-        //
+    public String [] RandLeveling() {
+        Letters = only.RanLetGen();
+        return Letters;
     }
 
-    public static boolean CheckLeveling(int Lvl) {
-        if (Lvl < 11) {
+    public boolean CheckLeveling(int Lvl) {
+        if (level < 11) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int getAtt() {
+        return tempts;
+    }
+
+    public int getPtsR() {
+        return pts_req;
+    }
+
+    public static void incLvl() {
+        level++;
+    }
+
+    public boolean CheckProgress (int Score) {
+        if (Score >= pts_req) {
+            incLvl();
             return true;
         }
         else {
