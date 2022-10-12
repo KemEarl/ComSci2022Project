affa4aee65122d32d400ae2e9a8b5e0e6644e75c
KemEarl <68971765+KemEarl@users.noreply.github.com>
2022-10-12 10:36:17 +0200

    Update Leveling.java

--- Leveling.java
+++ Leveling.java
@@ -2,21 +2,21 @@ public class Leveling {
     private static int pts_req;
     private static int tempts;
     private static int level;
-    private static int [] att = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30}; 
+    private static int [] att = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
     private static int [] pts = {200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000};
     private static String [][] let_me = {{"N","T","A","R","I","G","C"},{"A","S","O","H","W","P","L"},{"M","E","L","S","R","U","D"},{"G","A","C","S","F","D","H"},{"I","T","D","F","A","O","E"},{"C","P","S","H","R","E","O"},{"M","S","Y","A","T","R","E"},{"K","N","I","S","E","F","G"},{"O","U","V","L","B","D","N"},{"L","B","F","E","S","A","P"}};
     private static RandomLetGen only = new RandomLetGen();
 	private static String [] Letters;
     //double check this intialisation
 	/**
-	 * 
+	 *
 	 */
 	public Leveling() {
         pts_req = 0;
         tempts = 0;
         level = 1;
     }
-	
+
     public String [] FixedLeveling() {
 		pts_req = pts[level -1];
 		tempts = att[level - 1];
@@ -26,12 +26,12 @@ public class Leveling {
 
     public String [] RandLeveling() {
         pts_req += 200;
-		tempts += 3;
+		    tempts += 3;
         Letters = only.RanLetGen();
         return Letters;
     }
 
-    public boolean CheckLeveling(int Lvl) {
+    public static boolean CheckLeveling(int Lvl) {
         if (level < 10) {
             return true;
         }
